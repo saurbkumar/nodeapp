81cdbfd15861e2d92074ca491f9181567235453d
saurabhkumar.spsu@gmail.com <saurabhkumar.spsu@gmail.com>
2021-12-31 11:32:10 -0800

    code coverage command added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   env: {
-    // browser: true,
     node: true,
     commonjs: true,
-    mocha: true
-    // es2021: true
+    mocha: true,
+    es2021: true
   },
   extends: ['eslint:recommended', 'prettier'],
   parserOptions: {
--- api/controllers/helloController.js
+++ api/controllers/helloController.js
@@ -8,7 +8,7 @@ async function getHello(req, res) {
   try {
     const result = await service.getHello(req.query.name, req.query.age);
     return res.json(result);
-  } catch (error) {
+  } catch (error) /* istanbul ignore next */ {
     logger.error(`getHello: Error while getHello: ${error}`);
   }
 }
